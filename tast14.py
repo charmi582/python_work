d2233b44fe0fca672ddec74cfd9ee53eb0a04ed0
charmi <h927889932@gmail.com>
2025-04-07 18:11:24 +0800

    數字內部能出現偶數

--- tast14.py
+++ tast14.py
@@ -1,10 +1,14 @@
 def a(n):
-    b=[]
-    for y in range(n):
-        for i in str(n):
-            for j in i:
-                if int(j)%2==1:
-                    b.append(y)
-    return b
-n=int(input())
+    result = []
+    for i in range(1, n + 1):
+        all_odd = True  # 旗標，預設為全部都是奇數
+        for ch in str(i):
+            if int(ch) % 2 == 0:
+                all_odd = False
+                break  # 有偶數就跳出，不用看了
+        if all_odd:
+            result.append(i)
+    return result
+
+n = int(input())
 print(a(n))
