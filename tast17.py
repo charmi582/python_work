080cd9fc9de23720f20238575975c8829433b389
charmi <h927889932@gmail.com>
2025-04-08 20:38:49 +0800

    進階版鞏固題

---
+++ tast17.py
@@ -0,0 +1,16 @@
+def a(n):
+    b=[]
+    for i in range(1, n+1):
+        result=True
+        s=str(i)
+        for j in range(1, len(s)):
+            g=int(s[j-1])
+            g+=1
+            
+            if int(s[j-1])+1==int(s[j]):
+                result=False
+        if result:
+            b.append(i)
+    return b
+n=int (input())
+print(a(n))
