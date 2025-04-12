83d7b344bc752319d5a46b0982a1b351007e2d7c
charmi <h927889932@gmail.com>
2025-04-12 15:45:02 +0800

    點初點初

---
+++ tast28.py
@@ -0,0 +1,10 @@
+def a(n):
+    count={str(i):0 for i in range(10)}
+    for i in range(10):
+        for j in str(i):
+            count[j]+=1
+    max_count = max(count.values())
+    result=[k for k, v in count.items() if v == max_count]
+    return result
+n=int(input())
+print(a(n))
