cd09351475ebd52c8f864b1bb33e43e1c22398ee
charmi <h927889932@gmail.com>
2025-04-17 20:00:21 +0800

    字典

--- tast45.py
+++ tast45.py
@@ -3,7 +3,7 @@ def a(n):
     count={}
     for i in word:
         count[i]=count.get(i, 0)+1
-    max_count=max(count.keys(), key=lambda x:count[x])
-    return max_count
+    min_count=min(count.keys(), key=lambda x: count[x])
+    return min_count
 n=input()
 print(a(n))
