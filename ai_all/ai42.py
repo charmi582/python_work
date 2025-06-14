a80138cbd8d4b81012a1308c54fbe6eed47dedbc
charmi528 <h927889932@gmail.com>
2025-06-14 22:58:57 +0800

    ai

--- ai_all/ai42.py
+++ ai_all/ai42.py
@@ -1,5 +1,5 @@
 import numpy as np
-from sklearn.preprocessing import StandardScaler 
+from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from keras.utils import to_categorical
 from keras.models import Sequential
@@ -7,12 +7,20 @@ from keras.layers import Dense
 from keras.callbacks import EarlyStopping
 from sklearn.metrics import classification_report, confusion_matrix
 
-x=np.array([ [170, 70, 25, 45, 1], [165, 60, 20, 35, 3], [180, 85, 28, 50, 0],
-    [172, 75, 30, 40, 0], [160, 50, 18, 28, 5], [175, 78, 27, 38, 1],
-    [168, 68, 22, 32, 2], [185, 90, 29, 55, 0], [158, 48, 15, 25, 6],
-    [162, 52, 20, 30, 4], [177, 80, 26, 43, 1], [166, 58, 19, 34, 3],
-    [178, 83, 28, 46, 0], [163, 54, 17, 27, 5], [176, 79, 24, 41, 2]])
-y=np.array([1, 2, 0, 0, 2, 1, 1, 0, 2, 2, 1, 2, 0, 2, 1])
+x=np.array([[65, 60, 55, 85], [70, 65, 60, 90], [75, 70, 68, 95],
+    [50, 55, 52, 60], [55, 50, 45, 70], [45, 48, 50, 65],
+    [80, 85, 82, 98], [78, 82, 80, 95], [85, 88, 90, 99],
+    [60, 58, 62, 75], [65, 64, 63, 80], [68, 66, 65, 88],
+    [40, 42, 45, 55], [45, 44, 43, 60], [50, 49, 48, 70],
+    [90, 92, 94, 100], [88, 89, 90, 99], [85, 87, 88, 98]])
+y=np.array([
+    1, 1, 1,
+    0, 0, 0,
+    2, 2, 2,
+    1, 1, 1,
+    0, 0, 0,
+    2, 2, 2
+])
 
 scaler=StandardScaler()
 x_scaler=scaler.fit_transform(x)
@@ -22,8 +30,8 @@ y=to_categorical(y)
 x_train, x_test, y_train, y_test=train_test_split(x_scaler, y, test_size=0.3, stratify=y)
 
 model=Sequential([
-    Dense(10, activation='relu', input_shape=(5, )),
-    Dense(6, activation="tanh"),
+    Dense(10, activation='relu', input_shape=(4, )),
+    Dense(6, activation='relu'),
     Dense(3, activation='softmax')
 ])
 
@@ -43,5 +51,4 @@ y_testz=np.argmax(y_test, axis=1)
 y_predz=np.argmax(y_pred, axis=1)
 
 print(classification_report(y_testz, y_predz))
-
 print(confusion_matrix(y_testz, y_predz))
