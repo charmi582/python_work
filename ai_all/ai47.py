64433379a5c72d742c69c0eddbb21f451b25fae5
charmi528 <h927889932@gmail.com>
2025-06-20 23:09:03 +0800

    ai

--- ai_all/ai47.py
+++ ai_all/ai47.py
@@ -1,8 +1,10 @@
 import numpy as np
+from keras import Input
 from sklearn.metrics import confusion_matrix, classification_report
 from keras.utils import to_categorical
 from keras.datasets import cifar100
 from keras.layers import Dense, Conv2D, Normalization, Dropout, MaxPool2D, Flatten
+from keras.layers import RandomContrast, RandomFlip, RandomRotation, RandomZoom
 from keras.models import Sequential
 from sklearn.model_selection import train_test_split
 from keras.callbacks import EarlyStopping
@@ -18,12 +20,21 @@ x_test.astype('float32')/255.0
 
 x_train, x_val, y_train, y_val=train_test_split(x, y, test_size=0.3, random_state=42, stratify=y)
 
-y_test=to_categorical(y_test)
-y_train=to_categorical(y_train)
-y_val=to_categorical(y_val)
+y_test=to_categorical(y_test, num_classes=100)
+y_train=to_categorical(y_train, num_classes=100)
+y_val=to_categorical(y_val, num_classes=100)
+
+data=Sequential([
+    Input(shape=(32, 32, 3)),
+    RandomFlip('horizontal'),
+    RandomContrast(0.1), 
+    RandomRotation(0.1),
+    RandomZoom(0.1)
+])
 
 model=Sequential([
-    Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)),
+    data,
+    Conv2D(32, (3, 3), activation='relu'),
     Normalization(),
     MaxPool2D(2, 2),
     Conv2D(64, (3, 3), activation='relu'),
@@ -43,7 +54,7 @@ model=Sequential([
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 early_stop=EarlyStopping(monitor='val_accuracy', patience=5, restore_best_weights=True)
 
-model.fit(x_train, y_train, epochs=50, batch_size=100, verbose=1, callbacks=[early_stop],
+model.fit(x_train, y_train, epochs=10, batch_size=100, verbose=1, callbacks=[early_stop],
            validation_data=(x_val, y_val))
 
 loss, acc=model.evaluate(x_test, y_test)
